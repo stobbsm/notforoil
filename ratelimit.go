e1e70a97cddd06b7ff19389e7028a0e40a8f5fc1
Matthew Stobbs <stobbsm@gmail.com>
2021-04-07 22:30:15 -0600

    Initial Commit
    Nothing done yet.

---
+++ ratelimit.go
@@ -0,0 +1,19 @@
+package ratelimit
+
+import "time"
+
+type Ratelimiter struct {
+	t *time.Ticker
+}
+
+func NewLimiter(requests uint32, seconds time.Duration) *Ratelimiter {
+	return &Ratelimiter{t: time.NewTicker(seconds)}
+}
+
+func (r *Ratelimiter) tick(req uint32, sec time.Duration) {
+
+}
+
+func (r *Ratelimiter) Limit() {
+	<-r.t.C
+}
